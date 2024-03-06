cc7c66d5f0d1e2084aeac9041d7269e6df0b727d
Pavel Ježek <91-jezep8am@users.noreply.localhost>
2024-03-06 23:31:36 +0000

    DU1 kostra reseni

---
+++ 01-Physics/JumpingPlatformGameWinFormsApp/Entities.cs
@@ -0,0 +1,34 @@
+﻿namespace JumpingPlatformGame {
+
+	// TODO: Provide your implementation of the following classes using your types from the PhysicsUnitsLib project.
+	
+	class Entity {
+		public virtual Color Color => Color.Black;
+	}
+
+	class MovableEntity : Entity {
+	}
+
+	class MovableJumpingEntity : MovableEntity {
+	}
+
+	class Joe : MovableEntity {
+		public override string ToString() => "Joe";
+		public override Color Color => Color.Blue;
+	}
+
+	class Jack : MovableEntity {
+		public override string ToString() => "Jack";
+		public override Color Color => Color.LightBlue;
+	}
+
+	class Jane : MovableJumpingEntity {
+		public override string ToString() => "Jane";
+		public override Color Color => Color.Red;
+	}
+
+	class Jill : MovableJumpingEntity {
+		public override string ToString() => "Jill";
+		public override Color Color => Color.Pink;
+	}
+}
