8e8397aeaa47dc11a98001ccbacd7d5c92c478bb
lipatova <alipatova2003@gmail.com>
2024-03-10 18:59:56 +0100

    add meters extension

--- 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
+++ 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
@@ -30,5 +30,26 @@ namespace PhysicsUnitsLib
         {
             return new Meters(value);
         }
+
+        /// <summary>
+        /// return new Seconds-type instance with double value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Seconds Seconds(this double value)
+        {
+            return new Seconds(value);
+        }
+
+        /// <summary>
+        /// return new Seconds-type instance with double ( <= int) value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Seconds Seconds(this int value)
+        {
+            return new Seconds(value);
+        }
+
     }
 }
