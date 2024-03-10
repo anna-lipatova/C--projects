966c174e896256f1242842ae57226b4d8f2c9d25
lipatova <alipatova2003@gmail.com>
2024-03-10 18:33:29 +0100

    Add file MeasurementExtention to convert double and int into meters

---
+++ 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhysicsUnitsLib
+{
+    public static class MeasurementExtention
+    {
+        /// <summary>
+        /// return new Meters-type instance with double value
+        /// example:
+        /// var m = 1.2.Meters() return new Meters-type instance with 1.2 value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Meters Meters(this double value)
+        {
+            return new Meters(value);
+        }
+
+        /// <summary>
+        /// return new Meters-type instance with double( <= int) value
+        /// example:
+        /// var m = 1.Meters() return new Meters-type instance with 1.0 value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static Meters Meters(this int value)
+        {
+            return new Meters(value);
+        }
+    }
+}
