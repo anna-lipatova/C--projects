ac2f866c89bdae39532f8ebb8a0d5e30d8173512
lipatova <alipatova2003@gmail.com>
2024-03-10 19:36:17 +0100

    edit Measurement Extension: add MetersPerSeconds

--- 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
+++ 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
@@ -6,25 +6,28 @@ namespace PhysicsUnitsLib
 {
     public static class MeasurementExtention
     {
-
         /// <summary>
-        /// return new Seconds-type instance with double value
+        /// return new Meters-type instance with double value
+        /// example:
+        /// var m = 1.2.Meters() return new Meters-type instance with 1.2 value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Seconds Seconds(this double value)
+        public static Meters Meters(this double value)
         {
-            return new Seconds(value);
+            return new Meters(value);
         }
 
         /// <summary>
-        /// return new Seconds-type instance with double ( <= int) value
+        /// return new Meters-type instance with double( <= int) value
+        /// example:
+        /// var m = 1.Meters() return new Meters-type instance with 1.0 value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Seconds Seconds(this  int value)
-        { 
-            return new Seconds(value); 
+        public static Meters Meters(this int value)
+        {
+            return new Meters(value);
         }
 
         /// <summary>
@@ -46,6 +49,27 @@ namespace PhysicsUnitsLib
         {
             return new Seconds(value);
         }
+
+        /// <summary>
+        /// return new MetersPerSeconds-type instance with double value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static MetersPerSeconds MetersPerSeconds(this double value)
+        {
+            return new MetersPerSeconds(value);
+        }
+
+        /// <summary>
+        /// return new MetersPerSeconds-type instance with double ( <= int) value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static MetersPerSeconds MetersPerSeconds(this int value)
+        {
+            return new MetersPerSeconds(value);
+        }
+
 
     }
 }
