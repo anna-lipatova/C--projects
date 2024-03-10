5b877b6fcc79bc4cc82d4bd2ef8992ebdb665729
lipatova <alipatova2003@gmail.com>
2024-03-10 18:43:13 +0100

    Add Seconds struct and new MesurementExtention

--- 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
+++ 01-Physics/PhysicsUnitsLib/MeasurementExtention.cs
@@ -6,28 +6,25 @@ namespace PhysicsUnitsLib
 {
     public static class MeasurementExtention
     {
+
         /// <summary>
-        /// return new Meters-type instance with double value
-        /// example:
-        /// var m = 1.2.Meters() return new Meters-type instance with 1.2 value
+        /// return new Seconds-type instance with double value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Meters Meters(this double value)
+        public static Seconds Seconds(this double value)
         {
-            return new Meters(value);
+            return new Seconds(value);
         }
 
         /// <summary>
-        /// return new Meters-type instance with double( <= int) value
-        /// example:
-        /// var m = 1.Meters() return new Meters-type instance with 1.0 value
+        /// return new Seconds-type instance with double ( <= int) value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static Meters Meters(this int value)
-        {
-            return new Meters(value);
+        public static Seconds Seconds(this  int value)
+        { 
+            return new Seconds(value); 
         }
     }
 }
