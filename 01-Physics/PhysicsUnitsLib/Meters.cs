7b9c16908a79eb6119fe378eb54e061125ce6ad1
lipatova <alipatova2003@gmail.com>
2024-03-12 11:23:22 +0100

    edit overloaded operators due to adding implicit operators for int => Meters and double => Meters

--- 01-Physics/PhysicsUnitsLib/Meters.cs
+++ 01-Physics/PhysicsUnitsLib/Meters.cs
@@ -26,25 +26,27 @@ namespace PhysicsUnitsLib {
             return new Meters(left.Value + right.Value);
         }
 
-        public static Meters operator +(Meters left, double right)
-        {
-            return new Meters(left.Value + right);
-        }
+        //no need in these overloaded operators fro every case of parameter's type
+        //because there are implicit overload operators for int and double
+        //public static Meters operator +(Meters left, double right)
+        //{
+        //    return new Meters(left.Value + right);
+        //}
 
-        public static Meters operator +(double left, Meters right)
-        {
-            return new Meters (left + right.Value);
-        }
+        //public static Meters operator +(double left, Meters right)
+        //{
+        //    return new Meters (left + right.Value);
+        //}
 
-        public static Meters operator +(Meters left, int right)
-        {
-            return new Meters(left.Value + right);
-        }
+        //public static Meters operator +(Meters left, int right)
+        //{
+        //    return new Meters(left.Value + right);
+        //}
 
-        public static Meters operator +(int left, Meters right)
-        {
-            return new Meters(left + right.Value);
-        }
+        //public static Meters operator +(int left, Meters right)
+        //{
+        //    return new Meters(left + right.Value);
+        //}
 
         /// <summary>
         /// operator overload for difference
@@ -75,25 +77,27 @@ namespace PhysicsUnitsLib {
             return (left.Value < right.Value);
         }
 
-        public static bool operator >(Meters left, double right)
-        {
-            return left.Value > right;
-        }
+        //no need in these overloaded operators fro every case of parameter's type
+        //because there are implicit overload operators for int and double
+        //public static bool operator >(Meters left, double right)
+        //{
+        //    return left.Value > right;
+        //}
 
-        public static bool operator >(double left, Meters right)
-        {
-            return (left > right.Value);
-        }
+        //public static bool operator >(double left, Meters right)
+        //{
+        //    return (left > right.Value);
+        //}
 
-        public static bool operator <(Meters left, double right)
-        {
-            return (left.Value < right);
-        }
+        //public static bool operator <(Meters left, double right)
+        //{
+        //    return (left.Value < right);
+        //}
 
-        public static bool operator <(double left, Meters right)
-        {
-            return (left < right.Value);
-        }
+        //public static bool operator <(double left, Meters right)
+        //{
+        //    return (left < right.Value);
+        //}
 
         /// <summary>
         /// method overload Meters => ToString 
@@ -118,6 +122,22 @@ namespace PhysicsUnitsLib {
         }
 
 
+        /// <summary>
+        /// implicit overload int into Meters
+        /// </summary>
+        /// <param name="m"></param>
+        public static implicit operator Meters(int m)
+        {
+            return new Meters(m);
+        }
        
+        /// <summary>
+        /// implicit overload double into Meters
+        /// </summary>
+        /// <param name="m"></param>
+        public static implicit operator Meters(double m)
+        {
+            return new Meters(m);
+        }
     }
 }
