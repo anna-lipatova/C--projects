7278aa56266357101096dda9a3cc485cfdc7cadd
lipatova <alipatova2003@gmail.com>
2024-03-23 16:42:52 +0100

    Class or Struct? change to class, but hesitate

--- 02-FluentPanels/MinimalisticUIFramework/ControlExtensions.cs
+++ 02-FluentPanels/MinimalisticUIFramework/ControlExtensions.cs
@@ -34,11 +34,13 @@
     }
 
     /// <summary>
-    /// struct specifying the location of a control on a canvas
+    /// specifying the location of a control on a canvas // OR CLASS?????
     /// At return TControl object located on a canvas
     /// </summary>
     /// <typeparam name="TControl"></typeparam>
-    public struct CanvasLocationSettings<TControl>
+    /// 
+    public class CanvasLocationSettings<TControl>
+    //public struct CanvasLocationSettings<TControl>
         where TControl: Control
     {
         private Canvas _canvas;
