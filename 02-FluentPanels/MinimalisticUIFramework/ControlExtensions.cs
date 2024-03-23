eb2755a1d8e185fce0cf0d98d5ed8be7c92ea5a3
lipatova <alipatova2003@gmail.com>
2024-03-23 15:58:40 +0100

    Add ControlExtensions with Control Extension for StackPanel

---
+++ 02-FluentPanels/MinimalisticUIFramework/ControlExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinimalisticUIFramework
+{
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Control Extension for StackPanel
+        /// used when Image/Label/StackPanel/Canvas PLACED IN StackPanel
+        /// </summary>
+        /// <typeparam name="TControlChild"></typeparam>
+        /// <param name="_control"></param>
+        /// <param name="sP"></param>
+        /// <returns></returns>
+        public static TControlChild PlacedIn<TControlChild>(this TControlChild _control, StackPanel sP)
+            where TControlChild : Control
+        {
+            sP.AddChild(_control);
+            return _control;
+        }
+
+    }
+}
