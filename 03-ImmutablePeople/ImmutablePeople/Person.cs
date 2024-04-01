235a7a12a3e5bced1d4b165fa528ef975d8706f9
lipatova <alipatova2003@gmail.com>
2024-04-01 13:58:54 +0200

    Change accessibility of abstract class Person Change access type od Person prop Add Student class and Teacher class

---
+++ 03-ImmutablePeople/ImmutablePeople/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmutablePeople
+{
+    abstract public class Person
+    {
+        public string Name { get; }
+        public string Password { get; }
+
+    }
+}
---
+++ 03-ImmutablePeople/ImmutablePeople/StudentAndTeacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmutablePeople
+{
+    public class Student: Person
+    {
+        public DateTime DateEnrolled { get; init; }
+    }
+
+    public class Teacher: Person
+    {
+        public int CoursesHeld { get; init; }
+    }
+}
