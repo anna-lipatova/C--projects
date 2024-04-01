9a0e5b4698d154938ef8d3bb797e833cc0952ea6
lipatova <alipatova2003@gmail.com>
2024-04-01 13:58:54 +0200

    Add abstract class Person

---
+++ 03-ImmutablePeople/ImmutablePeople/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmutablePeople
+{
+    abstract class Person
+    {
+        protected string Name { get; }
+        protected string Password { get; }
+
+    }
+}
