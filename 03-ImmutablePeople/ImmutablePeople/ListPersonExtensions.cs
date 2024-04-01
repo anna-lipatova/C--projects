ca66b2d12c3ed2747d95e3705672b62c3babcb75
lipatova <alipatova2003@gmail.com>
2024-04-01 17:39:14 +0200

    Add ListPersonExtensions & List<Person>.PrintAll()

---
+++ 03-ImmutablePeople/ImmutablePeople/ListPersonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImmutablePeople
+{
+    public static class ListPersonExtensions
+    {
+        public static void PrintAll(this IEnumerable<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.GetType().Name} {person.Name} has password \"{person.Password}\"");
+            }
+        }
+    }
+}
