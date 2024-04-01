eea985e7b89d99785236b06a58ba2d8587ea6193
lipatova <alipatova2003@gmail.com>
2024-04-01 14:51:23 +0200

    Add default prop & ctor

--- 03-ImmutablePeople/ImmutablePeople/Person.cs
+++ 03-ImmutablePeople/ImmutablePeople/Person.cs
@@ -8,9 +8,23 @@ namespace ImmutablePeople
     /// i want only my children have access to me 
     /// </summary>
     /// <typeparam name="TPerson"></typeparam>
-    abstract public class Person<TPerson> where TPerson : Person<TPerson>
+    abstract public class Person<TPerson> where TPerson : Person<TPerson>, new()
     {
         public string Name { get; }
         public string Password { get; }
+
+        protected Person(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+
+        public static TPerson Default
+        {
+            get
+            {
+                return new TPerson();
+            }
+        }
     }
 }
