a9b8620a5c3164e63f94a5531eeae2fd3abb6598
lipatova <alipatova2003@gmail.com>
2024-04-01 18:48:39 +0200

    Add extention WithPasswordResetByFirstName

--- 03-ImmutablePeople/ImmutablePeople/ListPersonExtensions.cs
+++ 03-ImmutablePeople/ImmutablePeople/ListPersonExtensions.cs
@@ -13,5 +13,28 @@ namespace ImmutablePeople
                 Console.WriteLine($"{person.GetType().Name} {person.Name} has password \"{person.Password}\"");
             }
         }
+
+        public static List<TPerson> WithPasswordResetByFirstName<TPerson>(
+            this IEnumerable<TPerson> people,
+            string firstName,
+            string newPassword
+            )
+            where TPerson : Person
+        {
+            var list = new List<TPerson>();
+
+            string firstNameWithSpace = firstName + " ";
+
+            foreach ( var person in people )
+            {
+                if (person.Name.StartsWith(firstNameWithSpace))
+                {
+                    list.Add((TPerson)person.WithPassword(newPassword));
+                }
+                else { list.Add(person); }
+            }
+
+            return list;
+        }
     }
 }
