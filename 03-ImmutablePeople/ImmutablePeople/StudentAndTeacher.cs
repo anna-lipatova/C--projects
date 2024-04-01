7f5e87a26e8e366c356144491e78b7a04effcdc1
lipatova <alipatova2003@gmail.com>
2024-04-01 14:52:06 +0200

    Add ctor() & ctor

--- 03-ImmutablePeople/ImmutablePeople/StudentAndTeacher.cs
+++ 03-ImmutablePeople/ImmutablePeople/StudentAndTeacher.cs
@@ -9,7 +9,16 @@ namespace ImmutablePeople
     /// </summary>
     public class Student: Person<Student>
     {
-        public DateTime DateEnrolled { get; init; }
+        public DateTime DateEnrolled { get; }
+
+        public Student(string name, string password, DateTime dateEnrolled) : base(name, password)
+        {
+            DateEnrolled = dateEnrolled;
+        }
+
+        public Student() : this(string.Empty, string.Empty, default)
+        {
+        }
     }
 
     /// <summary>
@@ -18,5 +27,14 @@ namespace ImmutablePeople
     public class Teacher: Person<Teacher>
     {
         public int CoursesHeld { get; init; }
+
+        public Teacher() : this(string.Empty, string.Empty, 0)
+        {
+        }
+
+        public Teacher(string name, string password, int coursesHeld) : base(name, password)
+        {
+            CoursesHeld = coursesHeld;
+        }
     }
 }
