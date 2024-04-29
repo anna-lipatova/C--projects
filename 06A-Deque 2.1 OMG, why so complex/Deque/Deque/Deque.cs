2a199ed81b06d24be788203304c2b1488e51bad4
lipatova <alipatova2003@gmail.com>
2024-04-29 17:45:52 +0200

    Add implementation for IList<T> without GetEnumerator()

--- 06A-Deque 2.1 OMG, why so complex/Deque/Deque/Deque.cs
+++ 06A-Deque 2.1 OMG, why so complex/Deque/Deque/Deque.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 
 
 namespace Deque
 {
-    public sealed class Deque<T> : IList
+    public sealed class Deque<T> : IList, IList<T>
     {
-        private object[] _items = new object[8];
+        private const int DefaultCapacity = 4;
+
+        private T[] _items = new T[DefaultCapacity];
         private int _count;
 
         public Deque()
@@ -15,134 +18,172 @@ namespace Deque
             _count = 0;
         }
 
-        public object this[int index]
+        object? IList.this[int index]
         {
             get => _items[index];
-            set => _items[index] = value;
-        }
-
-        public int Count
-        {
-            get
+            set
             {
-                return _count;
-            }
-        }
+                if (value == null && default(T) != null)
+                    throw new ArgumentNullException(nameof(value));
 
-
-        public bool IsFixedSize
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public bool IsReadOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public bool IsSynchronized
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public object SyncRoot
-        {
-            get
-            {
-                return this;
-            }
-        }
-
-
-        //returns index of the added element (is the last one)
-        public int Add(object value)
-        {
-            if (_count < _items.Length)
-            {
-                _items[_count] = value;
-                _count++;
-
-                return (_count - 1);
-            }
-
-            return -1;
-        }
-
-        public void Clear()
-        {
-            _count = 0;
-        }
-
-        public bool Contains(object value)
-        {
-            for (int i = 0; i < Count; i++)
-            {
-                if (_items[i] == value)
+                try
                 {
-                    return true;
+                    this[index] = (T)value!;
+                }
+                catch (InvalidCastException)
+                {
+                    throw new ArgumentException(null, nameof(value));
                 }
             }
-            return false;
         }
 
-        public int IndexOf(object value)
+        public T this[int index]
         {
-            for (int i = 0; i < Count; i++)
+            get
             {
-                if (_items[i] == value)
-                {
-                    return i;
-                }
+                if (index > _count)
+                    throw new IndexOutOfRangeException();
+                return (T)_items[index];
             }
-            return -1;
-        }
-
-        public void Insert(int index, object value)
-        {
-            if ((_count + 1 <= _items.Length) && (index < Count) && (index >= 0))
+            
+            set
             {
-                _count++;
-
-                for (int i = Count - 1; i > index; i--)
-                {
-                    _items[i] = _items[i - 1];
-                }
+                if (index > _count)
+                    throw new IndexOutOfRangeException();
                 _items[index] = value;
             }
         }
 
-        public void Remove(object value)
+        public int Count => _count;
+
+        bool IList.IsFixedSize => false;
+
+        bool ICollection<T>.IsReadOnly => false;
+
+        bool IList.IsReadOnly => false;
+
+        bool ICollection.IsSynchronized => false;
+
+        object ICollection.SyncRoot => this;
+
+        public void Add(T value)
         {
-            RemoveAt(IndexOf(value));
+            if (_count == _items.Length)
+                IncreaseCapacity();
+            _items[_count++] = value;
+        }
+
+        //returns index of the added element (is the last one)
+        int IList.Add(object? value)
+        {
+            if (value == null && default(T) != null)
+                throw new ArgumentNullException(nameof(value));
+
+            try
+            {
+                Add((T)value!);
+            }
+            catch (InvalidCastException)
+            {
+                throw new ArgumentException(null, nameof(value));
+            }
+
+            return Count - 1;
+        }
+
+        public void Clear()
+        {
+            _items = new T[4];
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public bool Contains(object value)
+        {
+            return Contains((T)value);
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_items, item, 0, _count);
+        }
+
+        public int IndexOf(object value)
+        {
+            return IndexOf((T)value);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > _count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index must be within the bounds of the Deque.");
+            if (_count == _items.Length)
+                IncreaseCapacity();
+
+            if (index < _count)
+            {
+                Array.Copy(_items, index, _items, index + 1, _count - index);
+            }
+
+            _items[index] = item;
+            _count++;
+        }
+
+        public void Insert(int index, object value)
+        {
+            Insert(index, (T)value);
         }
 
         public void RemoveAt(int index)
         {
-            if ((index >= 0) && (index < Count))
+            if (index < 0 || index >= _count)
+                throw new ArgumentOutOfRangeException(nameof(index), "Index must be within the bounds of the Deque.");
+
+            for (int i = index; i < Count - 1; i++)
             {
-                for (int i = index; i < Count - 1; i++)
-                {
-                    _items[i] = _items[i + 1];
-                }
-                _count--;
+                Array.Copy(_items, index + 1, _items, index, _count - index - 1);
             }
+            _count--;
+            _items[_count] = default(T);
+        }
+
+        public void Remove(object value)
+        {
+            int index = IndexOf(value);
+            if (index != -1)
+                RemoveAt(index);
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index != -1)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(_items, 0, array, arrayIndex, _count);
         }
 
         public void CopyTo(Array array, int index)
         {
-            for (int i = 0; i < Count; i++)
-            {
-                array.SetValue(_items[i], index++);
-            }
+            CopyTo((T[])array, index);
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] newArray = new T[_items.Length * 2];
+            Array.Copy(_items, newArray, _count);
+            _items = newArray;
         }
 
         public IEnumerator GetEnumerator()
