e3e0f2b42cbc305374d52f51fadc3fc4ac743814
lipatova <alipatova2003@gmail.com>
2024-08-03 18:07:15 +0200

    implement TypeLimiter class

--- UniversalInventorySystem - project/UniversalInventorySystem/UniversalInventorySystemLibrary/Limiters/TypeLimiter.cs
+++ UniversalInventorySystem - project/UniversalInventorySystem/UniversalInventorySystemLibrary/Limiters/TypeLimiter.cs
@@ -7,14 +7,31 @@ using UniversalInventorySystemLibrary.Items;
 
 namespace UniversalInventorySystemLibrary.Limiters
 {
-    public class TypeLimiter : IContainerLimiter
+    public interface IItemWithType
     {
+        int GetTypeID();
+    }
+    public class TypeLimiter<EnumT> : IContainerLimiter where EnumT : Enum
+    {
+        private Dictionary<EnumT, int> _limits;
+
+        public TypeLimiter(Dictionary<EnumT, int> limits)
+        {
+            _limits = limits;
+        }
+    
         public bool CanAddItem(IItem item)
         {
-            throw new NotImplementedException();
+            IItemWithType itemWithType = item as IItemWithType;
+            if (itemWithType != null)
+            {
+                EnumT type = (EnumT)Enum.ToObject(typeof(EnumT), itemWithType.GetTypeID());
+            }
+
+            return false;
         }
 
-        public bool CanAddItemArray(IEnumerable<IItem> items, List<IItem> canAddItems, List<IItem> cannotAddItems)
+        public bool CanAddItemArray(ICollection<IItem> items, List<IItem> canAddItems, List<IItem> cannotAddItems)
         {
             throw new NotImplementedException();
         }
