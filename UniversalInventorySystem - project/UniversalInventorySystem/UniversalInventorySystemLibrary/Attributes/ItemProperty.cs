b619b90343c303a584ed51e91449b18c9d40f102
lipatova <alipatova2003@gmail.com>
2024-08-08 20:13:01 +0200

    add attributes

---
+++ UniversalInventorySystem - project/UniversalInventorySystem/UniversalInventorySystemLibrary/Attributes/ItemProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UniversalInventorySystemLibrary.Attributes
+{
+    public sealed class ItemProperty: Attribute
+    {
+        public bool Ignore {  get; private set; }
+
+        public ItemProperty(bool ignore)
+        {
+            Ignore = ignore;
+        }   
+    }
+}
---
+++ UniversalInventorySystem - project/UniversalInventorySystem/UniversalInventorySystemLibrary/Attributes/PropertyLocalizationKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace UniversalInventorySystemLibrary.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class PropertyLocalizationKey: Attribute
+    {
+        public string Key { get; private set; }
+
+        public PropertyLocalizationKey(string key)
+        {
+            Key = key;
+        }   
+    }
+}
