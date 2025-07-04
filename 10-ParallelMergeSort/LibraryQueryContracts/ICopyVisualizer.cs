89ddd34d5c26296378d19b1d6d64bd40781f0d34
Anna Lipatova <alipatova2003@gmail.com>
2025-07-04 14:56:51 +0200

    Delete 10-ParallelMergeSort directory

--- 10-ParallelMergeSort/LibraryQueryContracts/ICopyVisualizer.cs
+++
@@ -1,9 +0,0 @@
-﻿using LibraryModel;
-
-// IMPORTANT NOTE: You should NOT change this file as part of your solution! Put you implementation into MergeSortQuery project only.
-
-namespace LibraryQueryContracts {
-	public interface ICopyVisualizer {
-		public void PrintLoanedCopy(TextWriter writer, Copy copy);
-	}
-}
